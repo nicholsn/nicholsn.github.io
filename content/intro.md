a2cd61a3500958a2791e14f8fef45a01e1839979
Nolan Nichols <nolan.nichols@gmail.com>
2013-09-08 11:51:30 -0700

    add: templates and config for plumage theme

---
+++ content/intro.md
@@ -0,0 +1,10 @@
+Title: Introductory post
+Date: 2013-09-08 10:30
+Category: Python
+Tags: pelican, publishing
+Slug: intro
+Author: Nolan Nichols
+Summary: Introductory post
+
+Testing...
+==========
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,14 +3,21 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Nolan Nichols'
-SITENAME = u'Nolan-Nichols'
+SITENAME = u'Nolan Nichols'
+SITESUBTITLE= "ramblings of a neuroinformatician"
 SITEURL = 'http://nicholsn.github.io'
 
 TIMEZONE = 'America/Vancouver'
 
 DEFAULT_LANG = u'en'
 
-THEME = "niu-x2"
+THEME = "plumage"
+
+PLUGIN_PATH = "/Users/nolan/PycharmProjects/pelican-plugins"
+PLUGINS = ["neighbors", "related_posts", ]
+
+SITE_THUMBNAIL = "https://en.gravatar.com/userimage/26595965/475a270547e0d8313e94494e9a17f10f.png"
+SITE_THUMBNAIL_TEXT = "Nolan Nichols"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -21,20 +28,20 @@ TRANSLATION_FEED_ATOM = None
 LINKS =  (('Pelican', 'http://getpelican.com/'),
           ('Python.org', 'http://python.org/'),
           ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Github', 'https://github.com/nicholsn'),
+          ('LinkedIn', 'http://www.linkedin.com/in/nolannichols'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
-NIUX2_HEADER_SECTIONS = [ 
-     ("About", "about", "/pages/about.html", "icon-book"),
-     ("Research", "research", "/pages/research.html", "icon-beaker"),
-     ("Archives", "archives", "/archives.html", "icon-archive"),
-     ("Tags", "tags", "/tags.html", "icon-tag"),
-]
+# Menu Items
+MENUITEMS = []
+
+DISQUS_SITENAME = "nolan-nichols"
+
+GOOGLE_SEARCH = "001358792457409399469:hzfp1rcbkgw"
