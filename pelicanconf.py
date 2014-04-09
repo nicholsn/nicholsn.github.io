71cfc3069d428f76be77d06df766af883f71820c
Nolan Nichols <nolan.nichols@gmail.com>
2014-04-09 14:17:03 -0700

    add: disqus config

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+import os
+
 AUTHOR = u'Nolan Nichols'
 SITENAME = u'Nolan Nichols'
 #SITESUBTITLE = "musings of a neuroinformatician"
@@ -11,16 +13,13 @@ TIMEZONE = 'America/Vancouver'
 
 DEFAULT_LANG = u'en'
 
-THEME = "plumage"
+THEME = "pelican-bootstrap3"
 
 PLUGIN_PATH = "/Users/nolan/PycharmProjects/pelican-plugins"
-PLUGINS = ["neighbors", "related_posts",]
+PLUGINS = ["neighbors", "related_posts", "disqus_static"]
 
 STATIC_PATHS = ['images', 'extra/CNAME']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
-
-SITE_THUMBNAIL = "https://en.gravatar.com/userimage/26595965/475a270547e0d8313e94494e9a17f10f.png"
-SITE_THUMBNAIL_TEXT = "Nolan Nichols"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = False
@@ -28,9 +27,7 @@ CATEGORY_FEED_ATOM = False
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),)
+LINKS =  (('Pelican', 'http://getpelican.com/'))
 
 # Social widget
 SOCIAL = (('Github', 'https://github.com/nicholsn'),
@@ -44,7 +41,10 @@ RELATIVE_URLS = True
 # Menu Items
 MENUITEMS = []
 
-DISQUS_SITENAME = "nolan-nichols"
+# Disqus config
+DISQUS_SITENAME = "nicholsn"
+DISQUS_SECRET_KEY = open(os.path.join(os.path.expanduser('~'), '.disqus_secret_key')).read()
+DISQUS_PUBLIC_KEY = u'iSFOPGc1Dvv6EmPU01BRQt1bK9GproBDnPc5IVFLJj7ETeEFMXVM5YiXwcTcK3Bd'
 
 ARTICLE_EDIT_LINK = 'https://github.com/nicholsn/nicholsn.github.io/blob/gh-pages/content/%(slug)s.md'
 
