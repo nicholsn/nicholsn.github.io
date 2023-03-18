a6524b02be1aacfe07e4ee3705e50382cfa5fc6e
Nolan Nichols <nnichols@mazetx.com>
2023-03-18 12:13:24 -0700

    add: new pelican version

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,109 +1,34 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
+AUTHOR = 'Nolan Nichols'
+SITENAME = 'Nolan Nichols'
+SITEURL = ''
 
-import os
+PATH = 'content'
 
-AUTHOR = "Nolan Nichols"
-SITENAME = "Nolan Nichols"
-#SITESUBTITLE = "musings of a neuroinformatician"
-SITEURL = ""
+TIMEZONE = 'America/Los_Angeles'
 
-TIMEZONE = "America/Vancouver"
-
-DEFAULT_LANG = "en"
+DEFAULT_LANG = 'en'
 
 # Theme
-THEME = "/Users/nicholsn/Repos/pelican-themes/pelican-bootstrap3"
+THEME = "/Users/nnichols/Code/pelican-themes/elegant"
 
-# Theme specific config
-BOOTSTRAP_THEME = "slate"
-PYGMENTS_STYLE = 'solarizeddark'
-SITELOGO = ""
-SITELOGO_SIZE = ""
-HIDE_SITENAME = False
-#DISPLAY_BREADCRUMBS = True
-#DISPLAY_CATEGORY_IN_BREADCRUMBS = True
-BOOTSTRAP_NAVBAR_INVERSE = False
-FAVICON = "images/favicon.png"
-DISPLAY_ARTICLE_INFO_ON_INDEX = True
-ABOUT_ME = ""
-AVATAR = "https://en.gravatar.com/userimage/26595965/475a270547e0d8313e94494e9a17f10f.png?size=200"
-CC_LICENSE = "CC-BY"
-SHOW_ARTICLE_AUTHOR = True
-SHOW_ARTICLE_CATEGORY = True
-USE_PAGER = True
-BOOTSTRAP_FLUID = True
-RELATED_POSTS_MAX = 10
-USE_OPEN_GRAPH = True
-
-# Notebook Rendering
-NOTEBOOK_DIR = 'notebooks'
-EXTRA_HEADER = open('_nb_header.html').read().decode('utf-8')
-CUSTOM_CSS = 'static/custom.css'
-
-# Template settings
-DISPLAY_PAGES_ON_MENU = True
-DISPLAY_CATEGORIES_ON_MENU = False
-DIRECT_TEMPLATES = ('index', 'categories', 'authors', 'archives', 'search')
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),)
-TAG_CLOUD_MAX_ITEMS = 20
-DISPLAY_TAGS_ON_SIDEBAR = True
-DISPLAY_TAGS_INLINE = True
-DISPLAY_CATEGORIES_ON_SIDEBAR = True
-HIDE_SIDEBAR = False
-
-# Articles per page
-DEFAULT_PAGINATION = 10
-RECENT_POST_COUNT = 5
-
-# Plugins
-PLUGIN_PATHS = ["/Users/nicholsn/Repos/pelican-plugins"]
-PLUGINS = ['related_posts', 'tipue_search', 'liquid_tags.img',
-           'liquid_tags.video', 'liquid_tags.youtube',
-           'liquid_tags.vimeo', 'liquid_tags.include_code',
-           'liquid_tags.notebook']
-
-# Static paths and cname mapping
-PATH = "content"
-STATIC_PATHS = ['images', 'extra/CNAME', 'extra/custom.css']
-EXTRA_PATH_METADATA = {
-                       'extra/CNAME': {'path': 'CNAME'},
-                       'extra/custom.css': {'path': 'static/custom.css'}
-                       }
-PAGE_PATHS = ['pages']
-ARTICLE_PATHS = ['articles']
-ARTICLE_EXCLUDES = ['.']
-
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
 
 # Social widget
 SOCIAL = (('Github', 'https://github.com/nicholsn'),
           ('LinkedIn', 'http://www.linkedin.com/in/nolannichols'),)
 
-# Disqus config
-DISQUS_SITENAME = "nicholsn"
-
-# Addthis
-ADDTHIS_PROFILE = "ra-55f098e034ebcb96"
-ADDTHIS_DATA_TRACK_ADDRESSBAR = False
-
-# Github
-GITHUB_USER = "nicholsn"
-GITHUB_REPO_COUNT = 3
-GITHUB_SKIP_FORK = True
-GITHUB_SHOW_USER_LINK = True
-ARTICLE_EDIT_LINK = 'https://github.com/nicholsn/nicholsn.github.io/blob/gh-pages/content/%(slug)s.md'
-
-# Google registration
-GOOGLE_SEARCH = "001358792457409399469:hzfp1rcbkgw"
-GOOGLE_ANALYTICS_UNIVERSAL = "UA-49888620-1"
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = False
-CATEGORY_FEED_ATOM = False
-TRANSLATION_FEED_ATOM = None
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
--- publishconf.py
+++ publishconf.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
@@ -10,10 +6,16 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://nolan-nichols.com'
+# If your site is available via HTTPS, make sure SITEURL begins with https://
+SITEURL = 'https://www.nolan-nichols.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
+
+# Following items are often useful when publishing
+
+DISQUS_SITENAME = "nicholsn"
+#GOOGLE_ANALYTICS = ""
