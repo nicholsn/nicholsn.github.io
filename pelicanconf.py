b97533331045ca3896d4e1887789626c97fad086
Nolan Nichols <nnichols@mazetx.com>
2023-03-19 12:52:23 -0700

    add: CNAME to main

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,6 +11,15 @@ DEFAULT_LANG = 'en'
 # Theme
 THEME = "/Users/nnichols/Code/pelican-themes/elegant"
 
+# Elegant theme settings.
+TAGS_URL = "tags"
+CATEGORIES_URL = "categories"
+ARCHIVES_URL = "archives"
+ARTICLE_URL = "{slug}"
+PAGE_URL = "{slug}"
+PAGE_SAVE_AS = "{slug}.html"
+SEARCH_URL = "search"
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -22,10 +31,15 @@ AUTHOR_FEED_RSS = None
 LINKS = ((),)
 
 # Social widget
-SOCIAL = (('Github', 'https://github.com/nicholsn'),
-          ('LinkedIn', 'http://www.linkedin.com/in/nolannichols'),)
+SOCIAL = (("Email", "mailto:nolan.nichols@gmail.com"),
+          ('Github', 'https://github.com/nicholsn'),
+          ('LinkedIn', 'http://www.linkedin.com/in/nolannichols'),
+          ("RSS", SITEURL + "/feeds/all.atom.xml"),)
+SOCIAL_PROFILE_LABEL = u'Stay in Touch'
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+
+PLUGINS = ['webassets']
