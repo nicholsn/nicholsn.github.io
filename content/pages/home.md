3b818b51c595bcf3c1f0a4c899a5083e8acbee0e
Nolan Nichols <nnichols@mazetx.com>
2023-03-19 15:35:23 -0700

    add: updated home page message

--- content/pages/home.md
+++ content/pages/home.md
@@ -1,23 +1,22 @@
-Title:
+Title: Home Page    
 URL:
 Slug: home
 Save_as: index.html
 Status: hidden
 Author: Nolan Nichols <http://orcid.org/0000-0003-1099-3328>
 
-<table>
-    <tr>
-      <td style="text-align: center; padding-left: 25px;">
-        <img src="./images/nolan.png"></br>
-        </br>
-        Nolan Nichols, Ph.D.</br>
-        Associate Director</br>
-        Platform Engineering</br>
-        Maze Therapeutics</br>
-        <a href="mailto:nolan.nichols@gmail.com">nolan.nichols (at) gmail.com</a>
-      </td>
-      <td style="padding: 25px;">
-        <img src="./images/summerland.jpg">
-      </td>
-    </tr>
-</table>
+### Diving into Biotech Data Architecture and Intelligent Systems
+
+Are you interested in creating more trustworthy and reproducible results in the biotech
+industry? Look no further! My website focuses on the power of knowledge graphs and large
+language models working in concert for authoring, processing, and managing 
+biomedical knowledge, information, and data.
+
+By combining these tools, we can create a data-centric architecture that supports the
+creation of intelligent systems and cognitive AI agents. My goal is to empower
+scientists and researchers with these systems to use in their quest to create 
+trustworthy insights.
+
+Explore my website to learn more about how these technologies work and how they can
+benefit your research in healthcare and life sciences. Let's work together to create a 
+more intelligent and data-driven future!
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,17 @@ AUTHOR = 'Nolan Nichols'
 SITENAME = 'Nolan Nichols'
 SITEURL = 'https://www.nolan-nichols.com'
 
-PATH = 'content'
+# Static paths and cname mapping
+PATH = "content"
+STATIC_PATHS = ['images', 'extra/CNAME', 'extra/custom.css', 'extra/favicon.ico']
+EXTRA_PATH_METADATA = {
+    'extra/CNAME': {'path': 'CNAME'},
+    'extra/custom.css': {'path': 'static/custom.css'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+}
+PAGE_PATHS = ['pages']
+ARTICLE_PATHS = ['articles']
+ARTICLE_EXCLUDES = ['.']
 
 TIMEZONE = 'America/Los_Angeles'
 
@@ -19,6 +29,7 @@ ARTICLE_URL = "{slug}"
 PAGE_URL = "{slug}"
 PAGE_SAVE_AS = "{slug}.html"
 SEARCH_URL = "search"
+LANDING_PAGE_TITLE = "Blog"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -31,10 +42,10 @@ AUTHOR_FEED_RSS = None
 LINKS = ((),)
 
 # Social widget
-SOCIAL = (("Email", "mailto:nolan.nichols@gmail.com"),
+SOCIAL = (("Gmail", "mailto:nolan.nichols@gmail.com"),
           ('Github', 'https://github.com/nicholsn'),
           ('LinkedIn', 'http://www.linkedin.com/in/nolannichols'),
-          ("RSS", SITEURL + "/feeds/all.atom.xml"),)
+          ("RSS", f"{SITEURL}/feeds/all.atom.xml"),)
 SOCIAL_PROFILE_LABEL = u'Stay in Touch'
 
 DEFAULT_PAGINATION = 10
@@ -42,4 +53,6 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-PLUGINS = ['webassets']
+PLUGINS = ['webassets', 'tipue_search', 'liquid_tags']
+LIQUID_TAGS = ["img", "literal", "video", "youtube",
+               "vimeo", "include_code"]
